9cb87e1a7f50e29624a5fb64fa87604f229f6825
Face7or <jacksonleeper.jl@gmail.com>
2023-03-30 17:54:41 -0700

    blah

--- web-scrapy.go
+++ web-scrapy.go
@@ -2,9 +2,43 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
+	// Instantiate default collector
+	c := colly.NewCollector()
 
-	fmt.Println("Hello")
+	// On every a element which has href attribute call callback
+	/*c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		//c.Visit(e.Request.AbsoluteURL(link))
+	})*/
+	c.OnXML("*", func(e *colly.XMLElement) {
+		//link := e.Attr("div class")
+		// Print link
+		fmt.Printf("Link found: %q\n", e.Text)//, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		//c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Set error handler
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://enblocpress.com/developer-index/
+	//c.Visit("https://enblocpress.com/developer-index/")
+	c.Visit("https://odysee.com/@1pew2pew")
 }
